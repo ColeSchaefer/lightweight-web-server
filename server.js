42303930914e5b5298aee57a948f74bce61e4542
root <root@imbypass.pw>
2018-10-20 13:35:53 -0400

    v0.1.1

--- server.js
+++ server.js
@@ -1,27 +1,25 @@
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
-const config = require("./config/settings.json");
-const mimes = require('./config/filetypes.json');
-
-const rootDirectory = config.SERVER_ROOT;
+const config = require("./settings.json");
+const rootDirectory = config.server.root;
 
 http.createServer( function (request, response) {  
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
     
-    
     var fileExtArr = pathname.split('.');
     var fileExtension = fileExtArr[fileExtArr.length - 1];
+    var reqFile = rootDirectory + pathname.substr(1);
     
-    fs.readFile(rootDirectory + pathname.substr(1), function (err, data) {
+    fs.readFile(reqFile, function (err, data) {
         if (err) {
             if(err.code == 'EISDIR') {
-                fs.readFile(rootDirectory + pathname.substr(1) + '/index.html', function (errr, data) {
+                fs.readFile(reqFile + '/index.html', function (errr, data) {
                     if(errr) { 
                         if(errr.code == 'ENOENT') {
-                            response.writeHead(404, {'Content-Type': getMimeType('html')});
-                            response.write(createDirectoryListing(rootDirectory + pathname.substr(1), pathname, request));
+                            response.writeHead(200, {'Content-Type': getMimeType('html')});
+                            response.write(createDirectoryListing(reqFile, pathname, request));
                             response.end();
                         }
                     } else {
@@ -33,8 +31,9 @@ http.createServer( function (request, response) {
                 });
             }
             if(err.code == 'ENOENT') {
-                response.writeHead(404, {'Content-Type': getMimeType('html')});
-                response.write('PAGE NOT FOUND.');
+                let errorCode = 404;
+                response.writeHead(errorCode, {'Content-Type': getMimeType('html')});
+                response.write(fs.readFileSync('./templates/' + errorCode + '.html'));
                 response.end();
             }
         } else {
@@ -45,13 +44,13 @@ http.createServer( function (request, response) {
         }
     });
     
-}).listen(config.SERVER_PORT);
+}).listen(config.server.port);
 
-console.log('Listening on port '.concat(config.SERVER_PORT));
+console.log('Listening on port '.concat(config.server.port));
 
 function getMimeType(extension) {
-    if (mimes[extension]) return mimes[extension];
-    return mimes['bin'];
+    if (config.mimetypes[extension]) return config.mimetypes[extension];
+    return config.mimetypes['bin'];
 }
 function getBetween(content, start, end) {
   let arr = content.split(start);
@@ -64,18 +63,61 @@ function logWithTimestamp(data) {
   console.log('[*] ' + new Date(Date.now()).toLocaleTimeString('en-US') + ' - ' + data);
 }
 function createDirectoryListing(dir, pathname, req) {
+    var stringBuilder = fs.readFileSync(config.templates.index);
+    
+    var dirList = '';
     var dirArr = fs.readdirSync(dir);
-    var stringBuilder = '<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN"><html>' + 
-                        '<head><title>Index of ' + dir.replace(rootDirectory, '/') + '</title></head>' + 
-                        '<body><h1>Index of ' + dir.replace(rootDirectory, '/') + '</h1>' +
-                        '<table>';
     if(dirArr.length > 0) {
         dirArr.forEach(function(f) {
-            stringBuilder += '<tr><td><a href="http://' + req.headers.host + pathname.substr(1) + '/' + f + '">' + f + '</a></td></tr>';
+            let fName = rootDirectory + pathname.substr(1) + '/' + f;
+            let fInfo = fs.statSync(fName);
+            
+            console.log('File info for ' + fName + ':');
+            console.log(fInfo);
+            
+            dirList += '<tr>';
+            dirList += '<td>';
+            dirList += '<a href="http://' + req.headers.host +'/'+ pathname.substr(1) + '/' + f + '">';
+            dirList += f;
+            dirList += '</a>';
+            dirList += '</td>';
+            dirList += '<td style="float:right;">'
+            dirList +=  getPrettySize(fInfo.size);
+            dirList += '</td>';
+            dirList += '</tr>';
+            if(dirArr.length > 1) dirList += "\r\n";
         });
     } else {
-        stringBuilder += '<tr><td>Directory is empty.</td></tr>';
+        dirList = '<tr><td>Directory is empty.</td></tr>';
     }
-    stringBuilder += '</table></body></html>';
+    stringBuilder = stringBuilder.toString().replace(/{{DIRECTORY}}/g, pathname);
+    stringBuilder = stringBuilder.toString().replace(/{{LISTING}}/g, dirList);
     return stringBuilder;
+}
+function getPrettySize(size) {
+    let ext = 'bytes';
+    if (size > 1024) {
+        ext = 'kb';
+        size /= 1024;
+        
+        if (size > 1024) {
+            ext = 'MB';
+            size /= 1024;
+            
+            if (size > 1024) {
+                ext = 'GB';
+                size /= 1024;
+            }
+        }
+    }
+    return (roundTo(size, 2)).toString() + ' ' + ext.toString();
+}
+function roundTo(n, digits) {
+    if (digits === undefined)
+        digits = 0;
+    
+    var multiplicator = Math.pow(10, digits);
+    n = parseFloat((n * multiplicator).toFixed(11));
+    var test =(Math.round(n) / multiplicator);
+    return +(test.toFixed(digits));
 }
